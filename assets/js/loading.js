fad2107e02e2c0517093388f379427b28687dc5e
potatolee <dean19990226@gmail.com>
2022-11-21 11:27:09 +0800

    loading add test

---
+++ assets/js/loading.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container:  document.getElementById('loading-sign'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'GNsign_loading.json',
+  })
