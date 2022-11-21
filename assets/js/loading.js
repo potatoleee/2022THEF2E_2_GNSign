6b2aac0785fd79082eb54be0bde75994ba9cfdf3
potatolee <dean19990226@gmail.com>
2022-11-21 11:59:01 +0800

    lottie

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -5,3 +5,19 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'GNsign_loading.json',
   })
+
+
+  function loadingPen(){
+    document.querySelector("main").style.display = "none";
+    document.querySelector("header").style.display = "none";
+    document.querySelector(".loading").style.display = "block";
+    setTimeout(function () {
+            $(document).ready(function () {
+                document.querySelector(".loading").style.display = "none";
+                document.querySelector("main").style.display = "block";
+                document.querySelector("header").style.display = "block";
+            });
+    }, 2500);
+}
+
+loadingPen();
